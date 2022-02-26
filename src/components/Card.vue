c69a69f3884fca9c0a3bee8d366314e64f569188
wonsikgo <wonsickgo@gmail.com>
2022-02-27 00:49:22 +0900

    중첩라우팅

---
+++ src/components/Card.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    Card
+    <div>cid : {{ cid }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    cid() {
+      return this.$route.params.cid;
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ import VueRouter from "vue-router";
 import Home from "../components/Home.vue";
 import Login from "../components/Login.vue";
 import Board from "../components/Board.vue";
+import Card from "../components/Card.vue";
 import NotFound from "../components/NotFound.vue";
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
+  mode: "history",
   routes: [
     {
       path: "/",
@@ -20,7 +22,14 @@ const router = new VueRouter({
     {
       path: "/b/:bid",
       component: Board,
+      children: [
+        {
+          path: "c/:cid",
+          component: Card,
+        },
+      ],
     },
+
     {
       path: "*",
       component: NotFound,
