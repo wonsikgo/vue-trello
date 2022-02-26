03f603fb1b42066f991787e222d759e5a1c7e865
wonsikgo <wonsickgo@gmail.com>
2022-02-27 00:58:03 +0900

    라우터 변경에 따른 스피너 적용

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
     Board
-    <div>bid : {{ bid }}</div>
-    <router-link :to="`/b/${bid}/c/1`">Card 1</router-link> |
-    <router-link :to="`/b/${bid}/c/2`">Card 2</router-link>
-    <hr />
-    <router-view></router-view>
+    <div v-if="loading">Loading board...</div>
+    <div v-else>
+      <div>bid : {{ bid }}</div>
+      <router-link :to="`/b/${bid}/c/1`">Card 1</router-link> |
+      <router-link :to="`/b/${bid}/c/2`">Card 2</router-link>
+      <hr />
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
@@ -14,11 +17,21 @@ export default {
   data() {
     return {
       bid: 0,
+      loading: false,
     };
   },
   created() {
+    this.fetchData();
     this.bid = this.$route.params.bid;
-    console.log(this.$route.params.bid);
+  },
+  methods: {
+    fetchData() {
+      this.loading = true;
+      setTimeout(() => {
+        this.bid = this.$route.params.bid;
+        this.loading = false;
+      }, 1000);
+    },
   },
 };
 </script>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,15 +1,32 @@
 <template>
   <div>
     Card
-    <div>cid : {{ cid }}</div>
+    <div v-if="loading">loading card...</div>
+    <div v-else>cid : {{ cid }}</div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      loading: false,
+    };
+  },
   computed: {
     cid() {
       return this.$route.params.cid;
+    },
+  },
+  created() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      this.loading = true;
+      setTimeout(() => {
+        this.loading = false;
+      }, 1000);
     },
   },
 };
