2db12b7b27fde5b483b7f9be2dd8ec6c22a29027
wonsikgo <wonsickgo@gmail.com>
2022-02-27 00:33:14 +0900

    NavBar 추가

---
+++ src/components/NavBar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/Login">Login</router-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
