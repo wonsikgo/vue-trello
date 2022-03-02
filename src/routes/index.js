2d13e5d13cef84b4ff4df289ec8d28cf2958950a
wonsickgo <45756634+wonsikgo@users.noreply.github.com>
2022-03-02 16:39:14 +0900

    네비게이션 가드 적용

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,12 +8,19 @@ import NotFound from "../components/NotFound.vue";
 
 Vue.use(VueRouter);
 
+const requireAuth = (to, from, next) => {
+  const isAuth = localStorage.getItem("token");
+  const loginPath = `/login?Path=${encodeURIComponent(to.path)} `;
+  isAuth ? next() : next(loginPath);
+};
+
 const router = new VueRouter({
   mode: "history",
   routes: [
     {
       path: "/",
       component: Home,
+      beforeEnter: requireAuth,
     },
     {
       path: "/login",
@@ -22,10 +29,12 @@ const router = new VueRouter({
     {
       path: "/b/:bid",
       component: Board,
+      beforeEnter: requireAuth,
       children: [
         {
           path: "c/:cid",
           component: Card,
+          beforeEnter: requireAuth,
         },
       ],
     },
