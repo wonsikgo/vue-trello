73ec098af588b9aed6ff0e873e3580ce663afc64
wonsickgo <45756634+wonsikgo@users.noreply.github.com>
2022-03-10 17:01:44 +0900

    Dragula를 사용하여 카드 이동

--- src/components/AddCard.vue
+++ src/components/AddCard.vue
@@ -41,10 +41,20 @@ export default {
     onSubmit() {
       if (this.invalidInput) return;
       const { inputTitle, listId } = this;
-      this.ADD_CARD({ title: inputTitle, listId }).finally(() => {
+      const pos = this.newCardPos();
+      this.ADD_CARD({ title: inputTitle, listId, pos }).finally(() => {
         this.inputTitle = "";
         this.$emit("close");
       });
+    },
+    newCardPos() {
+      const curList = this.$store.state.board.lists.filter(
+        (l) => l.id === this.listId
+      )[0];
+      if (!curList) return 65535;
+      const { cards } = curList;
+      if (!cards.length) return 65535;
+      return cards[cards.length - 1].pos * 2;
     },
     setupClickOutside(el) {
       document.querySelector("body").addEventListener("click", (e) => {
